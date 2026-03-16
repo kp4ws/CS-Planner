7b0cd6dec52afae500cf0488828da8338ebd53c7
kp4ws <kentpawson123@gmail.com>
2026-03-16 11:13:03 -0700

    Working on base REST API setup with UI

---
+++ api/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers.auth import router as auth_router
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+#Register routers
+app.include_router(auth_router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -1,9 +1,35 @@
+"use client"
+import {useState} from 'react';
+import Link from "next/link"
+
 export default function Login() {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    const handleSubmit = (event: React.SubmitEvent) => {
+        event.preventDefault();
+        console.log("Login with: ", {username, password});
+    };
+
     return (
-        <div>
-            <p>
-                Login Screen
-            </p>
-        </div>
+        <main className="flex items-center justify-center h-screen">
+            <div className="bg-white p-8 rounded-lg shadow-md w-96">
+                <h1 className="text-4xl font-bold">Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor='username'>Username</label>
+                        <input id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+
+                    <div className="flex flex-col mb-4">
+                        <label htmlFor='password'>Password</label>
+                        <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+
+                    <button type="submit">Submit</button>
+                    <Link href="/register">Register</Link>
+                </form>
+            </div>
+        </main>
     );
 }
