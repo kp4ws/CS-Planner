9340fe6a4fd3d3e3f40f11896174dbd1a6a26f1b
kp4ws <kentpawson123@gmail.com>
2026-05-17 11:14:41 -0700

    Working on inventory page UI

--- web/app/inventory/page.tsx
+++ web/app/inventory/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { useInventory } from "@/hooks/features/use-inventory";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { Filter } from "lucide-react";
 import { logger } from "@/lib/logger";
 
 export default function InventoryPage() {
   const { items, categories, isLoading, error, deleteItem } = useInventory();
 
+  if (isLoading) {
+    // TODO: Refactor loading widget
+    return <p>Loading ...</p>;
+  }
+
   if (error) {
-    // TODO: Refactor error screen
+    // TODO: Refactor error widget
     return (
       <div className="">
         <h2 className="text-white">Failed to load inventory</h2>
@@ -17,19 +22,13 @@ export default function InventoryPage() {
     );
   }
 
-  if (!isLoading) {
-    logger.info(
-      "Category Titles:",
-      categories.map((c) => c.title),
-    );
-  } else {
-    logger.info("Data is loading");
-  }
-
   return (
     <div className="min-h-screen px-6 py-4">
       {/* Inventory Header */}
       <header className="flex justify-between items-center mb-6">
+        {/* TODO: Error/Warning bar at top */}
+        <div></div>
+
         {/* TODO: Consider hiding page headers on desktop screens */}
         <h1 className="text-lg md:text-4xl text-white font-bold">Inventory</h1>
 
@@ -46,11 +45,15 @@ export default function InventoryPage() {
 
       {/* CATEGORY SECTIONS */}
       <section className="flex flex-col items-start gap-4">
-        {categories.map((category) => (
-          <h1 key={category.id} className="text-xl font-semibold text-white">
-            {category.title}
-          </h1>
-        ))}
+        {categories.map(category => {
+          const categoryItems = items.filter(item => item.category_id === category.id);
+          
+          return (
+              <div key={category.id}>
+
+              </div>
+          );
+        })}
       </section>
 
       {/* FOOTER SECTION (ADD BUTTON) */}
